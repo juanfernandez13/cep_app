6dc9bcb188a2b47139af07c6d439faffba7c5fcf
Juan Pablo <juanp1310@outlook.com>
2023-02-03 19:52:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:cep_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
